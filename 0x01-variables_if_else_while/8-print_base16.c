732326161c87176f01fe013c7b98485ec9c2011e
mohamedabdelfttah <mohamedabdelfattah1230@gmail.com>
2023-03-17 09:07:21 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,16 +8,15 @@
  */
 int main(void)
 {
-  char ch;
-
-  for (ch = '0'; ch <= '9'; ch++)
-    {
-      putchar(ch);
-    }
-  for (ch = 'a'; ch <= 'f'; ch++)
-    {
-      putchar(ch);
-    }
-  putchar('\n');
-  return (0);
+char ch;
+for (ch = '0'; ch <= '9'; ch++)
+{
+putchar(ch);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
