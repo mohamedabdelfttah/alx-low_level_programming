e10af7a4ddedeb566e2fa71c6a54f378e12b8b19
mohamedabdelfttah <mohamedabdelfattah1230@gmail.com>
2023-03-29 14:40:57 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,11 +8,10 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i = 0;
+  int j,i = 0;
 while (dest[i] != '\0')
 i++;
 
-int j = 0;
 while (src[j] != '\0' && j < n)
 {
 dest[i] = src[j];
