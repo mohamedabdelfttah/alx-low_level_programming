38164b6280212cb33e53c7a6db8b46b4d29fdc1d
mohamedabdelfttah <mohamedabdelfattah1230@gmail.com>
2023-04-10 18:10:01 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: n args
+ * @argv: arr args
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+int cents, coins;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+
+cents = atoi(argv[argc - 1]);
+
+if (cents < 0)
+{
+printf("0\n");
+return (0);
+}
+
+coins = 0;
+
+coins += cents / 25;
+cents %= 25;
+
+coins += cents / 10;
+cents %= 10;
+
+coins += cents / 5;
+cents %= 5;
+
+coins += cents / 2;
+cents %= 2;
+
+coins += cents;
+
+printf("%d\n", coins);
+
+return (0);
+}
