30cbb4f4a74676c4ba483bdc24f5dad777ce93fe
mohamedabdelfttah <mohamedabdelfattah1230@gmail.com>
2023-04-13 16:18:52 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *malloc_checked - returns a pointer
+ *@b:intger
+ *Return:p
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *p = malloc(b);
+if (p == NULL)
+{
+exit(98);
+}
+return (p);
+}
