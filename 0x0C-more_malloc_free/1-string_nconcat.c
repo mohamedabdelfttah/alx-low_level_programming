4060ca64fbac5930fd649c2453950abcd1128441
mohamedabdelfttah <mohamedabdelfattah1230@gmail.com>
2023-04-13 16:41:44 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,7 +7,7 @@
  *Return:char pointer
  */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *p;
 unsigned int size1 = 0, size2 = 0, i;
