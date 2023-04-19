ed5b5d526ba7de8e4e7325ae4fc87cdbe160b63f
mohamedabdelfttah <mohamedabdelfattah1230@gmail.com>
2023-04-19 15:04:35 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,9 @@
 #include "function_pointers.h"
-
 /**
  *print_name - function takes in two arguments
  *@name:a char pointer name
  *@f:a function pointer takes a char pointer as an argument
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 f(name);
