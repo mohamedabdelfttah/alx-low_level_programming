74de7547b5598e60ea52ecb31c51ebb4cd5ac207
mohamedabdelfttah <mohamedabdelfattah1230@gmail.com>
2023-04-19 15:01:44 -0700

    CALC Function

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of arguments passed to the program
+ * @argv: An array containing the arguments passed to the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int x;
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+
+x = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+printf("%d\n", x);
+
+return (0);
+}
