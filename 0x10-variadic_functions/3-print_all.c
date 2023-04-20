d3912a67ff203fd8b429a6298ca6dba6c93e7dfa
mohamedabdelfttah <mohamedabdelfattah1230@gmail.com>
2023-04-20 13:58:37 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,41 +10,44 @@
 
 void print_all(const char * const format, ...)
 {
-int i = 0;
-char *str;
-char *separator = "";
-
 va_list args;
-va_start(args, format);
+unsigned int i = 0, j, c = 0;
+char *str;
+const char t_arg[] = "cifs";
 
-while (format[i])
+va_start(args, format);
+while (format && format[i])
 {
+j = 0;
+while (t_arg[j])
+{
+if (format[i] == t_arg[j] && c)
+{
+printf(", ");
+break;
+} j++;
+}
 switch (format[i])
 {
 case 'c':
-printf("%s%c", separator, va_arg(args, int));
-separator = ", ";
+printf("%c", va_arg(args, int)), c = 1;
 break;
 case 'i':
-printf("%s%d", separator, va_arg(args, int));
-separator = ", ";
+printf("%d", va_arg(args, int)), c = 1;
 break;
 case 'f':
-printf("%s%f", separator, va_arg(args, double));
-separator = ", ";
+printf("%f", va_arg(args, double)), c = 1;
 break;
 case 's':
-str = va_arg(args, char *);
+str = va_arg(args, char *), c = 1;
 if (!str)
 {
 printf("(nil)");
 break;
 }
-printf("%s%s", separator, str);
+printf("%s", str);
 break;
+} i++;
 }
-i++;
-}
-printf("\n");
-va_end(args);
+printf("\n"), va_end(args);
 }
