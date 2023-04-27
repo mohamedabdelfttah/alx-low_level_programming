c7f93ae2611c6e595440f881a05f2e91694562bb
mohamedabdelfttah <mohamedabdelfattah1230@gmail.com>
2023-04-27 15:48:12 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,9 @@
 #ifndef _NODE
 #define _NODE
-#include <stdio.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
