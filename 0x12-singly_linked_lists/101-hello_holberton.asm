89fc5b24898986dafd8b7786d1f869787635140e
mohamedabdelfttah <mohamedabdelfattah1230@gmail.com>
2023-04-27 18:52:42 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,23 +1,15 @@
-; File: hello.asm
-; Assembly program to print "Hello, Holberton" using printf
-
-
-        section .data
-msg     db 'Hello, Holberton', 0
+	SECTION .data
+msg:	db "Hello, Holberton", 0
 fmt:	db "%s", 10, 0
 
-        section .text
+	SECTION .text
 	extern printf
 	global main
 main:
-        ; Prepare arguments for printf
-        mov     rdi, msg        ; Set first argument to address of message string
-        xor     rax, rax        ; Clear rax register (no floating point arguments)
+	mov esi, msg
+	mov edi, fmt
 	mov eax, 0
+	call printf
 
-        ; Call printf function
-        call    printf
-
-        ; Exit program
 	mov eax, 0
-        ret                     ; Return from main function
+	ret
